37782792d37263f61ae78e0490cf07d7fd28c6e6
bcaudillo <btcaudillo@gmail.com>
2022-04-18 17:40:43 -0600

    commit tree de2074dd93ef932a226f5b1c958189bfb98a2abf

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const main =main.remove();
+const newHeader=document.createElement("h1");
+const h1 = newHeader.id='victory';
+const victory = newHeader.innerHTML='is the champion'
